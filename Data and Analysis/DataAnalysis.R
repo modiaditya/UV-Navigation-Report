a1dab7a54d09a992d14d995370099eee8c578a53
modiaditya <aditya.ucla@gmail.com>
2012-11-28 17:28:01 -0800

    commiting data

---
+++ Data and Analysis/DataAnalysis.R
@@ -0,0 +1,36 @@
+route <- read.csv("/Users/Aditya/Documents/Development/LaTeX/UV\ Paper/Data\ and\ Analysis/route2.csv", header=FALSE , col.names= c("lat", "long","uva","uvb"), sep=",", skip=1 )
+for(ss in 1:10){
+  sample = ss
+  uvb = route$uvb
+  uvb10 <- vector()
+  for(i in 1:10){
+    uvbSample = sample(uvb,sample)
+    trueMean = mean(route$uvb)
+    sampleMean = mean(uvbSample)
+    t = abs(100-abs((trueMean-sampleMean)/ sampleMean*100))
+    uvb10[i]=t
+  }
+  cat("UVB Sample Size is ",sample)
+  cat("\n")
+  cat("UVB mean accuracy is ",mean(uvb10))
+  cat("\n")
+  #print(summary(uvb10))
+  sampleA = ss
+  
+  
+  
+  uva = route$uva
+  uva10 <- vector()
+  for(i in 1:10){
+    uvaSample = sample(uva,sampleA)
+    trueMeanA = mean(route$uva)
+    sampleMeanA = mean(uvaSample)
+    tA = abs(100-abs((trueMeanA-sampleMeanA)/ sampleMeanA*100))
+    uva10[i]=tA
+  }
+  cat("UVA Sample Size is ",sampleA)
+  cat("\n")
+  cat("UVA mean accuracy is ",mean(uva10))
+  cat("\n")
+  #print(summary(uva10))
+}
